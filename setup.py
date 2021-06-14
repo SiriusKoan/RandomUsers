1f84df1b15806cb6fa7fe4780d3565dda52d4717
SiriusKoan <k.peihsun@gmail.com>
2021-06-14 23:06:07 +0800

    Package basic structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RandomUsers",
+    description="RandomUsers",
+    long_description=open("README.txt").read(),
+    version="0.1.0",
+    author="SiriusKoan",
+    author_email="k.peihsun@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/SiriusKoan/RandomUsers",
+    license="LICENSE",
+    install_requires=[],
+)
